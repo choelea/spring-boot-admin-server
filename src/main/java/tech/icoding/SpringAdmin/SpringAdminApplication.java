3364b12f7cbf0897a67ee83e7e8e74c0912cf86f
Joe Li <joe.lea@foxmail.com>
2022-03-27 21:52:29 +0800

    init

---
+++ src/main/java/tech/icoding/SpringAdmin/SpringAdminApplication.java
@@ -0,0 +1,15 @@
+package tech.icoding.SpringAdmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class SpringAdminApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAdminApplication.class, args);
+	}
+}
