cff6c21ac57e1a626e1f226e3b98da62fe46012e
Joe Li <joe.lea@foxmail.com>
2022-03-27 22:07:10 +0800

    add script

---
+++ dockerImageDeloy.sh
@@ -0,0 +1,13 @@
+#！ /bin/bash
+# 定义变量
+namespace=joe
+image=spring-boot-admin-server
+username=choelea #dockerhub用户名
+password=$1 #dockerhub 密码
+tag=$2
+
+mvn clean package -Dmaven.test.skip=true
+docker login --username=$username --password=$password  # 使用注册的docker id登录；密码就是登陆否则登录hub.docker.com网站的密码
+docker build -t $namespace/$image:$tag .
+docker tag $namespace/$image:$tag $namespace/$image:$tag
+docker push $namespace/$image:$tag
